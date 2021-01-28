b0e1036058e5d1166b0c174bf5c9d337065314e3
Alan Oliveira <alancjobm@gmail.com>
2021-01-28 00:04:58 -0300

    move directories with rename

--- Rename/ex01.php
+++ Rename/ex01.php
@@ -15,10 +15,17 @@ if (!is_dir($dir2)) mkdir($dir2);
 
 if (!file_exists($dir1 . DIRECTORY_SEPARATOR . $filename)) 
 {
-    $file = fopen('./'. $dir . DIRECTORY_SEPARATOR . $filename, "w+");
+    $file = fopen($dir1 . DIRECTORY_SEPARATOR . $filename, "w+");
 
     fwrite($file, date("Y-m-d H:i:s"));
     fclose($file);
 }
+
+rename(
+    $dir1 . DIRECTORY_SEPARATOR . $filename, // Origem
+    $dir2 . DIRECTORY_SEPARATOR . $filename // Destino
+);
+
+echo "Arquivo movido com sucesso";
 
 ?>
